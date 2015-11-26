3ae7d160d176c58d84f7f2ca91a45b8b0ec77dc5
dearshrewdwit <dearshrewdwit@gmail.com>
2015-11-26 20:10:25 +0000

    added reviews and associations and passing rspec

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+
+gem "shoulda"
 gem 'haml'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,42 @@
 class RestaurantsController < ApplicationController
+
   def index
-    raise 'Hello from the index action'
-  end  
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to restaurants_path
+  end
+
+  def show
+    @restaurants = Restaurant.all
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurants_path
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully'
+    redirect_to restaurants_path
+  end
+
+  def restaurant_params
+   params.require(:restaurant).permit(:name)
+  end
+
 end
