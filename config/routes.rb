a69504d9d17eca4247f5a0433c44d3d1d4ec924b
dearshrewdwit <dearshrewdwit@gmail.com>
2015-11-27 10:49:07 +0000

    added facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  resources :restaurants do
+    resources :reviews
+  end
+
+  get 'restaurants' => 'restaurants#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,10 +13,8 @@ Rails.application.routes.draw do
   root to: "restaurants#index"
 
   # Example of regular route:
-  get 'restaurants' => 'restaurants#index'
-  resources :restaurants do
-    resources :reviews
-  end
+
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
