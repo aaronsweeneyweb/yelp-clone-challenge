e697aa6ab80b27b37e0d36b942d0dcf7eef23bec
dearshrewdwit <dearshrewdwit@gmail.com>
2015-11-27 10:09:39 +0000

    added user, devise, passing rspec

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem "devise"
 gem "shoulda"
 gem 'haml'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: "restaurants#index"
 
   # Example of regular route:
-    get 'restaurants' => 'restaurants#index'
-    resources :restaurants do
-      resources :reviews
-    end
+  get 'restaurants' => 'restaurants#index'
+  resources :restaurants do
+    resources :reviews
+  end
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
